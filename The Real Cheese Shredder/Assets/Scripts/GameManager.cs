a65f443a6ada3e4387f5f6fc1cb637b9764ee0f0
Suhaila Dehis <suhaila.dehis@maugames.com>
2019-12-26 11:50:24 +0200

    main ui ( tap to play , game over and restart game panel)

---
+++ The Real Cheese Shredder/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    #region Public Variables
+    public static GameManager instance;
+    public GameObject startgamePanel, gameOverPanel;
+    public ShreddingScript shreddingScript;
+
+
+    
+    #endregion
+
+    #region Public Methods
+    public void StartGame()
+    {
+        startgamePanel.SetActive(false);
+        shreddingScript.cheeseSpeed = 5;
+    }
+
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    #endregion
+
+
+    #region Unity Callbacks
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    #endregion
+}
--- The Real Cheese Shredder/Assets/Scripts/ShreddingScript.cs
+++ The Real Cheese Shredder/Assets/Scripts/ShreddingScript.cs
@@ -5,21 +5,20 @@ using UnityEngine;
 public class ShreddingScript : MonoBehaviour
 {
     #region Testing Variables
-    public bool isMoving; // is the cheese moving
     public float decreaseMargin = 0.05f;// how much the cheese will decrease over speed
     public float cheeseSpeed = 5f;
     public float minimumScaleToFinish = 0.3f;
     #endregion
 
     #region Private Variables
-    float originalScale=2.5f;
-    float orginalZ,originalY;
+    float originalScale = 2.5f;
+    float orginalZ, originalY;
 
     #endregion
 
 
     #region public variables
-
+    public GameObject endPointObject;
     #endregion
 
     #region Public Methods
@@ -27,7 +26,7 @@ public class ShreddingScript : MonoBehaviour
     public void ShredCheeseByScale()
     {
         // implementation 1 , decrease the scale constantly * time.deltatime
-        float newScale = this.gameObject.transform.localScale.y - (decreaseMargin * Time.deltaTime* cheeseSpeed);
+        float newScale = this.gameObject.transform.localScale.y - (decreaseMargin * Time.deltaTime * cheeseSpeed);
         this.transform.localScale = new Vector3(originalScale, newScale, originalScale);
     }
 
@@ -36,6 +35,11 @@ public class ShreddingScript : MonoBehaviour
         // implementation 1 , decrease the scale constantly * time.deltatime
         float newX = this.gameObject.transform.position.x + (decreaseMargin * Time.deltaTime * cheeseSpeed);
         this.transform.position = new Vector3(newX, originalY, orginalZ);
+
+        if(this.transform.position.x > endPointObject.transform.position.x)
+        {
+            GameManager.instance.GameOver();
+        }
     }
 
     #endregion
@@ -50,7 +54,11 @@ public class ShreddingScript : MonoBehaviour
     }
     private void Update()
     {
-        if (isMoving && this.transform.localScale.y>minimumScaleToFinish)
+        //if (cheeseSpeed > 0 && this.transform.localScale.y > minimumScaleToFinish)
+        //{
+        //    ShredCheeseByScale();
+        //}
+        if (cheeseSpeed > 0 && this.transform.position.x < endPointObject.transform.position.x)
         {
             ShredCheeseByTransform();
         }
